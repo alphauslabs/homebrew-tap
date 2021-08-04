8ee503caa9d387521bb8b2d4607b90c32613a2b5
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-04 11:20:11 +0900

    Brew formula update for bluectl version v0.6.0

--- Formula/bluectl.rb
+++ Formula/bluectl.rb
@@ -5,20 +5,20 @@
 class Bluectl < Formula
   desc "The official command line interface for Alphaus services."
   homepage "https://github.com/alphauslabs/bluectl"
-  version "0.5.9"
+  version "0.6.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/alphauslabs/bluectl/releases/download/v0.5.9/bluectl_0.5.9_darwin_amd64.tar.gz"
-      sha256 "6fb9568171841cdd2fafd166fbbddfbd94929502804724d78f04951e8dccbc91"
+      url "https://github.com/alphauslabs/bluectl/releases/download/v0.6.0/bluectl_0.6.0_darwin_amd64.tar.gz"
+      sha256 "ccb8b4a5925e04e461f4c04474d6fd144a351b6659a68576871b3d80bb8fe2ca"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/alphauslabs/bluectl/releases/download/v0.5.9/bluectl_0.5.9_linux_amd64.tar.gz"
-      sha256 "1cceba17fa6baaee150336f170ec0071106c063b2fd37580ccaacd929636b6ad"
+      url "https://github.com/alphauslabs/bluectl/releases/download/v0.6.0/bluectl_0.6.0_linux_amd64.tar.gz"
+      sha256 "784529f3a66c940ccb08dc0e97ad75e095da6c4d0a15b23806a5f587252b06d9"
     end
   end
 
