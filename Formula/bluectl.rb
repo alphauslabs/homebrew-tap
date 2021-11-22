4bd1f1bd0e51f99a273ba797ce42acab2e699f1e
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-22 12:23:47 +0900

    Brew formula update for bluectl version v0.14.4

--- Formula/bluectl.rb
+++ Formula/bluectl.rb
@@ -5,12 +5,12 @@
 class Bluectl < Formula
   desc "The official command line interface for Alphaus services."
   homepage "https://github.com/alphauslabs/bluectl"
-  version "0.14.3"
+  version "0.14.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/alphauslabs/bluectl/releases/download/v0.14.3/bluectl_0.14.3_darwin_amd64.tar.gz"
-      sha256 "6d485ef5911c53f1c0cd5b67b1e70e032c4aefa550390a6590e0b01f65fe1f31"
+      url "https://github.com/alphauslabs/bluectl/releases/download/v0.14.4/bluectl_0.14.4_darwin_amd64.tar.gz"
+      sha256 "e01447633cf6a7de1f5eb24cb2308166c193ed607a7b2e6faa46999f0e73e5b0"
 
       def install
         bin.install "bluectl"
@@ -20,8 +20,8 @@ class Bluectl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/alphauslabs/bluectl/releases/download/v0.14.3/bluectl_0.14.3_linux_amd64.tar.gz"
-      sha256 "db90cbc67170c5a0f0318f69aade49bd28c4e0a8702c64e866f8d8ed8f33fb58"
+      url "https://github.com/alphauslabs/bluectl/releases/download/v0.14.4/bluectl_0.14.4_linux_amd64.tar.gz"
+      sha256 "2dfe0429984a2dd8a8b6d53d4a605ba96e4487961122c10eea376969d74b461c"
 
       def install
         bin.install "bluectl"
